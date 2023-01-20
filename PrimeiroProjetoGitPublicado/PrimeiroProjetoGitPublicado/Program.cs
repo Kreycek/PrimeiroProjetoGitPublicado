7d86aa1d969f680155e13cca433db359e317dabb
kreycek <kreycek@gmail.com>
2023-01-20 13:10:20 +0000

    First commit

---
+++ PrimeiroProjetoGitPublicado/PrimeiroProjetoGitPublicado/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PrimeiroProjetoGitPublicado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
